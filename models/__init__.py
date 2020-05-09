3e405bb74c2fb7e510848181e7ef6139a6a39fbf
John Doe <anongithubber@gmail.com>
2020-05-09 18:03:19 +0800

    Enabled GCN(D)

--- models/__init__.py
+++ models/__init__.py
@@ -5,6 +5,7 @@ import glob
 from .DGCNN import DGCNN
 from .DiffPool import DiffPool
 from .GCN import GCN
+from .GCND import GCND
 
 modules = glob.glob(join(dirname(__file__), "*.py"))
 __all__ = [ basename(f)[:-3] for f in modules if isfile(f) and not f.endswith('__init__.py')]
